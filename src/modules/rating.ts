bb1b93dedeea4f2dc44e8b5476e2c0f62fc5790c
cucumberian <smamarsello@gmail.com>
2023-07-09 15:23:24 +0300

    feat: Rating RatingManager

---
+++ src/modules/rating.ts
@@ -0,0 +1,60 @@
+interface RatingParams {
+    id: number;
+    rating: number;
+    bookId: number;
+    userId: number;
+}
+
+class Rating implements RatingParams {
+    id: number;
+    rating: number;
+    bookId: number;
+    userId: number;
+    
+    constructor(ratingParams: RatingParams) {
+        for (let [key, value] of Object.entries(ratingParams)) {
+            this[key] = value;
+        }
+    }
+}
+
+class RatingManager {
+    ratings: Map<number, Rating> = new Map();
+
+    createNewRating(ratingParams: Omit<RatingParams, "id">): Rating {
+        const lastId = this.ratings.size;
+        const rating = new Rating({id: lastId, ...ratingParams});
+        return rating;
+    }
+
+    setRating(rating: Rating) {
+        this.ratings.set(rating.id, rating);
+    }
+
+    removeRating(ratingId): boolean {
+        if (this.ratings.has(ratingId)) {
+            this.ratings.delete(ratingId);
+            return true;
+        }
+        return false;
+    }
+
+    getAllRatings(): Rating[] {
+        return Object.values(Object.fromEntries(this.ratings));
+    }
+
+    findRatings(ratingParams: Partial<Omit<RatingParams, "id">>): Rating[] {
+        let ratings = this.getAllRatings();
+        for (let [key, value] of Object.entries(ratingParams)) {
+            ratings = ratings.filter( rating => rating[key] === value);
+        }
+        return ratings;
+    }
+
+    getBookAverageRating(bookid): number {
+        const bookratings = this.findRatings({bookId: bookid.id});
+        return bookratings.reduce((sum, rating) => sum + rating.rating , 0) / bookratings.length;
+    }
+}
+
+export {RatingParams, Rating, RatingManager };
